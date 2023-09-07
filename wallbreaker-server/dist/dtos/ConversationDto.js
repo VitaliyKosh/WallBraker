d95909e4ee197b0bae4eaf12a8f4adcf4a3161ae
VitaliyKosh <116172695+VitaliyKosh@users.noreply.github.com>
2023-09-07 09:35:29 +0300

    Delete wallbreaker-server/dist directory

--- wallbreaker-server/dist/dtos/ConversationDto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ConversationDto {
-    constructor(model) {
-        this.name = model.name;
-        this.id = model._id.toString();
-    }
-}
-exports.default = ConversationDto;
